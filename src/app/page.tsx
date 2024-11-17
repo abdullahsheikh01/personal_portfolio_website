f7d3e5498d541d11ee0e00033fc0ffe3045d40e4
abdullahsheikh01 <162831270+abdullahsheikh01@users.noreply.github.com>
2024-11-17 11:34:52 +0500

    Delete some useless code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,4 @@
-import Navbar from "./components/navbar/page";
 import Hero from "./components/hero/page";
-import About_Me from "./components/about_me/page";
-import My_Skills from "./components/my_skills/page";
-import MyWork from "./components/my_work/page";
-import Contact_Me from "./components/contact_me/page";
-import Footer from "./components/footer/page";
 export default function Home(){
   return(
     <div className="2xl:w-full md:w-screen xl:w-screen overflow-x-hidden">
